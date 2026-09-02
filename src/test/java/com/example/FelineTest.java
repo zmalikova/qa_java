965a755475b6bfc4f15890e9921303d3db66369c
malikova_z <zukhra.malikova.s@kaspi.kz>
2026-09-02 19:42:05 +0500

    add jacoco report

---
+++ src/test/java/com/example/FelineTest.java
@@ -0,0 +1,44 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
+public class FelineTest {
+
+    @Test
+    public void eatMeatShouldReturnPredatorFood() throws Exception {
+        Feline feline = new Feline();
+
+        List<String> expected = List.of("Животные", "Птицы", "Рыба");
+
+        assertEquals(expected, feline.eatMeat());
+    }
+
+    @Test
+    public void getFamilyShouldReturnCats() {
+        Feline feline = new Feline();
+
+        assertEquals("Кошачьи", feline.getFamily());
+    }
+
+    @Test
+    public void getKittensShouldReturnOne() {
+        Feline feline = new Feline();
+
+        assertEquals(1, feline.getKittens());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 5, 10})
+    public void getKittensShouldReturnSpecifiedNumber(int kittensCount) {
+        Feline feline = new Feline();
+
+        assertEquals(kittensCount, feline.getKittens(kittensCount));
+    }
+
+}
+
